ae7d0e3dac193669297e9f12b5c7ee5acd0fdf9b
Manish Shrestha <manishshtestha472@gmail.com>
2025-10-26 10:38:02 +0545

    Update ElevatedBtn widget to allow nullable onPressed callback and improve code formatting

--- lib/widgets/elevated_btn.dart
+++ lib/widgets/elevated_btn.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 // Provides customization for color, text, border radius, and padding
 class ElevatedBtn extends StatelessWidget {
   final String text; // Text displayed on the button
-  final VoidCallback onPressed; // Function executed when button is pressed
+  final VoidCallback? onPressed; // Function executed when button is pressed
   final Color? color; // Background color of the button
   final Color? textColor; // Text color
   final double borderRadius; // Rounds the button corners
@@ -13,9 +13,9 @@ class ElevatedBtn extends StatelessWidget {
   const ElevatedBtn({
     super.key,
     required this.text,
-    required this.onPressed,
-    this.color , // Default button color
-    this.textColor , // Default text color
+    this.onPressed,
+    this.color, // Default button color
+    this.textColor, // Default text color
     this.borderRadius = 12, // Default rounded corners
     this.padding = 16, // Default padding
   });
@@ -26,17 +26,27 @@ class ElevatedBtn extends StatelessWidget {
       width: double.infinity, // Makes button take full available width
       child: Padding(
         // Adds padding around the button for spacing from edges
-        padding: const EdgeInsetsDirectional.only(start: 12, end: 12, bottom: 8),
-        
+        padding: const EdgeInsetsDirectional.only(
+          start: 12,
+          end: 12,
+          bottom: 8,
+        ),
+
         // The main ElevatedButton widget
         child: ElevatedButton(
           // Styling for the button
           style: ElevatedButton.styleFrom(
-            backgroundColor: color ?? Theme.of(context).primaryColor, // Button background color
-            foregroundColor: textColor ?? Colors.white,// Text/icon color
-            padding: EdgeInsets.symmetric(vertical: padding), // Vertical padding
+            backgroundColor:
+                color ??
+                Theme.of(context).primaryColor, // Button background color
+            foregroundColor: textColor ?? Colors.white, // Text/icon color
+            padding: EdgeInsets.symmetric(
+              vertical: padding,
+            ), // Vertical padding
             shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(borderRadius), // Rounded corners
+              borderRadius: BorderRadius.circular(
+                borderRadius,
+              ), // Rounded corners
             ),
           ),
 
